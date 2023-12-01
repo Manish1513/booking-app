8dda59b355457eb640bc7fdc9e36b036c27c87da
manish1513 <gurjarmanish1513@gmail.com>
2023-12-01 14:54:42 +0530

    new js file

--- main.js
+++ main.js
@@ -1,48 +1,54 @@
 document.addEventListener('DOMContentLoaded', function () {
+  displayAppointments();
+});
+
+function bookAppointment() {
+  const name = document.getElementById('name').value;
+  const date = document.getElementById('date').value;
+  const time = document.getElementById('time').value;
+
+  if (name && date && time) {
+    // Retrieve existing appointments from local storage
+    const existingAppointments = JSON.parse(localStorage.getItem('appointments')) || [];
+
+    // Add the new appointment
+    const newAppointment = {
+      name: name,
+      date: date,
+      time: time,
+    };
+
+    existingAppointments.push(newAppointment);
+
+    // Save the updated appointments to local storage as a JSON string
+    localStorage.setItem('appointments', JSON.stringify(existingAppointments));
+
+    // Clear form fields
+    document.getElementById('name').value = '';
+    document.getElementById('date').value = '';
+    document.getElementById('time').value = '';
+
+    // Refresh the displayed appointments
     displayAppointments();
-  });
-  
-  function bookAppointment() {
-    const name = document.getElementById('name').value;
-    const date = document.getElementById('date').value;
-    const time = document.getElementById('time').value;
-  
-    if (name && date && time) {
-      const appointment = {
-        name: name,
-        date: date,
-        time: time,
-      };
-      const existingAppointments = JSON.parse(localStorage.getItem('appointments')) || [];
-      existingAppointments.push(appointment);
-      localStorage.setItem('appointments', JSON.stringify(existingAppointments));
-  
-      
-      document.getElementById('name').value = '';
-      document.getElementById('date').value = '';
-      document.getElementById('time').value = '';
-  
-      
-      displayAppointments();
-    } else {
-      alert('Please fill in all fields.');
-    }
+  } else {
+    alert('Please fill in all fields.');
   }
-  
-  function displayAppointments() {
-    const appointmentsList = document.getElementById('appointmentsList');
-    appointmentsList.innerHTML = '';
-  
-    const appointments = JSON.parse(localStorage.getItem('appointments')) || [];
-  
-    if (appointments.length === 0) {
-      appointmentsList.innerHTML = '<li>No appointments booked yet.</li>';
-    } else {
-      appointments.forEach(function (appointment) {
-        const listItem = document.createElement('li');
-        listItem.textContent = `${appointment.name} - Date: ${appointment.date}, Time: ${appointment.time}`;
-        appointmentsList.appendChild(listItem);
-      });
-    }
+}
+
+function displayAppointments() {
+  const appointmentsList = document.getElementById('appointmentsList');
+  appointmentsList.innerHTML = '';
+
+  // Retrieve existing appointments from local storage
+  const appointments = JSON.parse(localStorage.getItem('appointments')) || [];
+
+  if (appointments.length === 0) {
+    appointmentsList.innerHTML = '<li>No appointments booked yet.</li>';
+  } else {
+    appointments.forEach(function (appointment) {
+      const listItem = document.createElement('li');
+      listItem.textContent = `${appointment.name} - Date: ${appointment.date}, Time: ${appointment.time}`;
+      appointmentsList.appendChild(listItem);
+    });
   }
-  
+}
